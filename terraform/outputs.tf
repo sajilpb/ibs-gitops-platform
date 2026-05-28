106d4634c84bc308d781acb7e87020d0755d7738
sajil <sajil>
2026-05-29 00:43:56 +0530

    fix

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,8 +2,3 @@ output "elasticache_redis_endpoints" {
   description = "ElastiCache Redis endpoint details keyed by environment"
   value       = module.elastic-cache.redis_endpoints
 }
-
-output "external_secrets_role_arn" {
-  description = "IAM role ARN used by External Secrets Operator via IRSA"
-  value       = module.external_secrets.external_secrets_role_arn
-}
